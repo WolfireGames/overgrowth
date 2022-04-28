114bc24e38efa98ddc954cd7b95c4c263bf01064
Stephan Vedder <vedder@mbits.info>
2022-04-28 10:23:19 +0200

    Remove unused nvidia texture tools

--- Libraries/nvidia-texture-tools-read-only/src/nvcore/RefCounted.cpp
+++
@@ -1,9 +0,0 @@
-// This code is in the public domain -- castanyo@yahoo.es
-
-#include "RefCounted.h"
-
-using namespace nv;
-
-int nv::RefCounted::s_total_ref_count = 0;
-int nv::RefCounted::s_total_obj_count = 0;
-
